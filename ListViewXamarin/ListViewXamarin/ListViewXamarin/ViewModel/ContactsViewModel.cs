e08e87ef35dd10b79fea10da5ddb6353ced0f060
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-03-25 17:24:45 +0530

    XAMARIN-20195 - Sample added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/ViewModel/ContactsViewModel.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class ContactsViewModel : INotifyPropertyChanged
+    {
+        #region Properties 
+        public ObservableCollection<Contacts> contactsinfo { get; set; }
+        public Command<object> TapCommand { get; set; }
+        public Command<object> DoneCommand { get; set; }
+        public Contacts TappedItem
+        {
+            get => _tappedItem;
+            set {
+                _tappedItem = value;
+                OnPropertyChanged("TappedItem");
+            }
+        }
+        #endregion
+
+        #region Constructor
+
+        public ContactsViewModel()
+        {
+            contactsinfo = new ObservableCollection<Contacts>();
+            TapCommand = new Command<object>(buttonTapped);
+            DoneCommand = new Command<object>(doneTapped);
+            Random r = new Random();
+            for (int i = 0; i < CustomerNames.Count(); i++)
+            {
+                var contact = new Contacts(CustomerNames[i], r.Next(720, 799).ToString() + " - " + r.Next(3010, 3999).ToString());
+                contactsinfo.Add(contact);
+            }
+        }
+
+        private void doneTapped(object obj)
+        {
+            App.Current.MainPage.Navigation.PopAsync();
+        }
+
+        private void buttonTapped(object obj)
+        {
+            TappedItem = obj as Contacts;
+            var dataForm = new EditPage();
+            dataForm.BindingContext = this;
+            App.Current.MainPage.Navigation.PushAsync(dataForm);
+        }
+
+        #endregion
+
+        #region Fields
+
+        string[] CustomerNames = new string[] {
+            "Kyle",
+            "Gina",
+            "Irene",
+            "Katie",
+            "Jack",
+            "Liz",
+            "Zeke",
+            "Noah",
+            "Pete",
+            "Bill",
+            "Daniel",
+            "Frank",
+            "Brenda",
+            "Danielle",
+            "Fiona",
+            "Howard",
+            "Jack",
+            "Larry",
+            "Holly",
+            "Jennifer",
+            "Liz",
+            "Pete",
+            "Steve",
+            "Vince",
+            "Zeke",
+            "Aiden",
+            "Jackson",
+            "Mason",
+            "Liam",
+            "Jacob",
+            "Jayden",
+            "Ethan",
+            "Noah",
+            "Lucas",
+            "Logan",
+            "Caleb",
+            "Caden",
+            "Jack",
+            "Ryan",
+            "Connor",
+            "Michael",
+            "Elijah",
+            "Brayden",
+            "Benjamin",
+            "Nicholas",
+            "Alexander",
+            "William",
+            "Matthew",
+            "James",
+            "Landon",
+            "Nathan",
+            "Dylan",
+            "Evan",
+            "Luke",
+            "Andrew",
+            "Gabriel",
+            "Gavin",
+            "Joshua",
+            "Owen",
+            "Daniel",
+            "Carter",
+            "Tyler",
+            "Cameron",
+            "Christian",
+            "Wyatt",
+            "Henry",
+            "Eli",
+            "Joseph",
+            "Max",
+            "Isaac",
+            "Samuel",
+            "Anthony",
+            "Grayson",
+            "Zachary",
+            "David",
+            "Christopher",
+            "John",
+            "Isaiah",
+            "Levi",
+            "Jonathan",
+            "Oliver",
+            "Chase",
+            "Cooper" ,
+            "Tristan",
+            "Colton",
+            "Austin",
+            "Colin",
+            "Charlie",
+            "Dominic",
+            "Parker",
+            "Hunter",
+            "Thomas",
+            "Alex",
+            "Ian",
+            "Jordan",
+            "Cole",
+            "Julian",
+            "Aaron",
+            "Carson",
+            "Miles",
+            "Blake",
+            "Brody",
+            "Adam",
+            "Sebastian",
+            "Adrian",
+            "Nolan",
+            "Sean",
+            "Riley",
+            "Bentley",
+            "Xavier",
+            "Hayden",
+            "Jeremiah",
+            "Jason",
+            "Jake",
+            "Asher",
+            "Micah",
+            "Jace",
+            "Brandon",
+            "Josiah",
+            "Hudson",
+            "Nathaniel",
+            "Bryson",
+            "Ryder",
+            "Justin",
+            "Bryce",
+        };
+        private Contacts _tappedItem;
+
+        #endregion
+
+        #region Interface Member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
